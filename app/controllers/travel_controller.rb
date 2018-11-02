a30f473119b7da15951e5431bfa339b2e2f6c815
Josely Castro <joselycas@gmail.com>
2018-11-02 14:40:14 -0400

    cleaned up travel controller new route

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -1,33 +1,28 @@
 class TravelController < ApplicationController
 
   get '/travels' do
-    if logged_in?
-      @travels = Travel.all
-      erb :'/travels/show'
-    else
-      redirect to "/login"
-    end
+    authorize_user
+    @travels = Travel.all
+    erb :'/travels/show'
   end
 
   get '/travels/new' do
-   if logged_in?
-     erb :'/travels/new'
-    else
-      redirect to '/login'
-    end
+   authorize_user
+   erb :'/travels/new'
   end
 
   post '/travels' do
-    if logged_in? && params[:travels] != ""
-      @travel = current_user.travels.create(:location => params[:location], :activity => params[:activity], :travel_date => params[:travel_date])
-      redirect to "/travels/#{@travel.id}"
+    authorize_user
+      if params[:location] != "" && params[:activity] != "" && params[:date] != ""
+        @travel = current_user.travels.create(:location => params[:location], :activity => params[:activity], :travel_date => params[:travel_date])
+        redirect to "/travels/#{@travel.id}"
     else
-      redirect to '/travels/new'
+        redirect to '/travels/new'
     end
   end
 
   get '/travels/:id' do
-    authorize_user
+   authorize_user
    @travel = Travel.find(params[:id])
     if @travel.user_id == current_user.id
       erb :'/travels/travel'
@@ -69,20 +64,3 @@ class TravelController < ApplicationController
       "/travels/#{@travel.id}"
     end
 end
-
-
-
-
-#  patch '/travels/:id' do
-#   if logged_in?
-#     @travel = Travel.find(params[:id])
-#     #@travel.has_required_fields?
-#     #if @travel.has_required_fields?
-#   if @travel.user_id == current_user.id && params[:location] != "" && params[:activity] != "" && params[:date] != ""
-#      @travel.update(:location => params[:location], :activity => params[:activity], :travel_date => params[:travel_date])
-#      redirect to "/travels/#{@travel.id}"
-#    else
-#      erb :'/travels/error'
-#   end
-#  end
-# end
